daa22295e36510d0ac8d958542666d95aba86024
Yoaz Menda <yoazmenda@gmail.com>
2022-11-17 10:44:27 +0200

    include external file

--- src/boot_sect.asm
+++ src/boot_sect.asm
@@ -3,9 +3,7 @@
 mov bp, 0x8000
 mov sp, bp
 
-
 mov ax, welcome_string
-%include 'print.asm'
 call print_str
 
 jmp $ ; Jump to the current address ( i.e. forever ).
@@ -17,6 +15,7 @@ jmp $ ; Jump to the current address ( i.e. forever ).
 welcome_string:
     db 'Welcome to Yoaz OS', 0 
 
+%include 'print.asm'
 
 times 510 -( $ - $$ ) db 0 ; Pad the boot sector out with zeros
 dw 0xaa55 ; Last two bytes form the magic number ,
