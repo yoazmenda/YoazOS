b27cc13f8759a1be40ebdab44d685f0f7ae0a2a1
Yoaz Menda <yoazmenda@gmail.com>
2022-11-17 10:56:10 +0200

    change from floppy to hard disk

--- debug.sh
+++ debug.sh
@@ -4,6 +4,6 @@ mkdir -p out
 
 nasm src/boot_sect.asm -f bin -o out/boot_sect.bin -isrc/
 
-qemu-system-i386 -fda out/boot_sect.bin -gdb tcp::9000 -S &
+qemu-system-i386 -hda out/boot_sect.bin -gdb tcp::9000 -S &
 
 # (gdb) target remote localhost:9000
--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ mkdir -p out
 
 nasm src/boot_sect.asm -f bin -o out/boot_sect.bin -isrc/
 
-qemu-system-i386 -fda out/boot_sect.bin
+qemu-system-i386 -hda out/boot_sect.bin
