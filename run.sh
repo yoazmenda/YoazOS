13c87c646f7ddfbd321da9eb59f4a79d65342707
Yoaz Menda <yoazmenda@gmail.com>
2022-11-25 18:36:38 +0200

    improve run script

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+cd lib
+cd tinyemu-2019-12-21
+make clean
+make 
+cd ../..
+
+cp lib/tinyemu-2019-12-21/temu bin/temu
+chmod +x bin/temu
+
 mkdir -p out
 
 nasm src/boot_sect.asm -f bin -o out/boot_sect.bin -isrc/
