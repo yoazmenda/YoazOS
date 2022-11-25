165e89396b55b1f2068635a650a7c47021afd134
Yoaz Menda <yoaz.menda@gong.io>
2022-11-25 16:00:11 +0000

    add temu executable compiled from source

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ mkdir -p out
 
 nasm src/boot_sect.asm -f bin -o out/boot_sect.bin -isrc/
 
-qemu-system-i386 -hda out/boot_sect.bin
+# qemu-system-i386 -hda out/boot_sect.bin
+
+./bin/temu config.json
